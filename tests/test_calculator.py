9b3825a54d15385221d71dd76e2e1b6fc172193d
ahg3 <78547748+ahg3@users.noreply.github.com>
2022-09-27 20:06:20 -0400

    Update test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -6,11 +6,4 @@ def test_calculator_operations():
     assert Calculator.addition(2, 2) == 4, "The Addition Function Passed"
     assert Calculator.divide(2, 2) == 1, "The Division Function Passed"
     assert Calculator.multiply(2, 2) == 4, "The Multiplication Function Worked"
-    assert Calculator.subtract(2, 2) == 0, "The Subtraction Function Worked"
-
-    import sys
-
-
-def print_python_version():
-    """Function printing python version."""
-    print(sys.version)
+    assert Calculator.subtract(2, 2) == 0, "The Subtraction Function Worked
